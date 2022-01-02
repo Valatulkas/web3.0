cf14b1ffebbe1d1d773ccf71919c8c5cf2f33285
jmferg11 <jfergie93@gmail.com>
2022-01-02 09:12:19 -0500

    cleanse default react directories and add basic file structure

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Footer } from './footer.js';
+export { default as Loader } from './loader.js';
+export { default as Navbar } from './navbar.js';
+export { default as Services } from './services.js';
+export { default as Transactions } from './transactions.js';
+export { default as Welcome } from './welcome.js';
